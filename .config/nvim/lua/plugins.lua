d44f51714c3f0678a4cb8a53927981211dc264db
Mario Nieto Perez <mario.perez@frontiersin.org>
2022-08-03 09:37:30 +0200

    reorder properlyu

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,10 @@
 require("nvim-tree").setup()
+require 'colorizer'.setup()
 require("lualine").setup()
-
+require'nvim-web-devicons'.setup {
+    default = false;
+}
+-- Default options:
 require('kanagawa').setup({
     undercurl = true,           -- enable undercurls
     commentStyle = { italic = true },
@@ -18,5 +22,30 @@ require('kanagawa').setup({
     colors = {},
     overrides = {},
 })
+require("nvim-autopairs").setup {}
 
-require('nvim-autopairs').setup()
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"python","sql","hcl","dockerfile","bash","json","markdown","yaml","c","cpp"},
+
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    disable = {},
+    additional_vim_regex_highlighting = false,
+  },
+}
+require('Comment').setup()
+-- Line indent plugin
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
